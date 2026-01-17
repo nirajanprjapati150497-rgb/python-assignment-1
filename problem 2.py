4cebd734ee7f249c45e67ff4fcc328967523b01f
niranjan prajapati <nirajanprjapati150497@gmail.com>
2026-01-17 16:33:03 +0530

    Add files via upload

---
+++ problem 2.py
@@ -0,0 +1,6 @@
+#greeting program
+
+first_name=input("Enter your first name : ")
+last_name=input("Enter your last name : ")
+last_name=last_name+"!"
+print("Hello,",first_name,last_name,"Welcome to the python program.")
